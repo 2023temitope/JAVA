1f00e57b51d9c191c30645fdc92fee47ba6ce324
2023temitope <topbeemak@yahoo.com>
2024-01-12 11:50:37 +0000

    newjava

---
+++ src/demo/App.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class App {
+public static void main(String[] args) {
+	byte byt = 25;
+	short shrt = 255;
+	int i =2_100_000_000;
+	// without the L Java thinks it's an int
+	long l =999999999999999L;
+	
+	float f = 44.94F;
+	double d= 44.94;
+	
+	boolean b = false;
+	// ''  for chars and ""for  strings
+	char c ='j';
+	String s= "Jordan";
+	
+	
+	s=null;
+}
+
+
+
+}
